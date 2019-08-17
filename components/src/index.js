3e380950f48948a9c5151b590949a06e44827544
Thomas Lu <thomaslu@vt.edu>
2019-08-17 13:56:43 -0400

    Through lesson 36

--- components/src/index.js
+++ components/src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
-    return <div>Hi there!</div>
+    return (
+        <div className = "ui container comments">
+            <CommentDetail 
+                author="Sam" 
+                timeAgo="Today at 4:45 PM" 
+                content="Nice blog post!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail 
+                author="Thomas" 
+                timeAgo="Today at 2:13 AM" 
+                content="I like the subject!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail 
+                author="Dennis" 
+                timeAgo="Yesterday at 5:00 PM" 
+                content="I like the writing"
+                avatar={faker.image.avatar()}
+            />
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));

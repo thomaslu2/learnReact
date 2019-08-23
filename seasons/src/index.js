b371ed1e151a1532faa4713f6a648a9fc85087f9
Thomas Lu <thomaslu@vt.edu>
2019-08-23 16:25:51 -0400

    Through lesson 66

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { lat: null, errorMessage: ''};
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
     }
 
     //React says we have to define render
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude});
-            },
-            err => {
-                this.setState({ errorMessage: err.message });
-            }
-        );
-        return <div>Latitude: {this.state.lat} </div>;
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+        if(!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>
+        }
+
+        return <div>Loading!</div>;
     }
 }
 

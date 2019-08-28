338121c429e532e8c3b6bd5fff62b68b8dc19046
Thomas Lu <thomaslu@vt.edu>
2019-08-27 21:58:32 -0400

    Through lesson 91

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import axios from 'axios';
 
-const App = () => {
-    return (
-        <div className="ui container" style={{marginTop: '10px'}}>
-            <SearchBar />
-        </div>
-    );
-};
+class App extends React.Component {
+    async onSearchSubmit(term) {
+        const response = await axios
+            .get('https://api.unsplash.com/search/photos', {
+                params: { query: term },
+                headers: {
+                    Authorization: 'Client-ID 2ebd98c2fdf73064aee01fee3bc602f9518f7292e68aa479f60f0de422a52a10'
+                }
+            });
+        console.log(response.data.results);
+    }
+    render(){
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+            </div>
+        );
+    }
+}
 
 export default App;

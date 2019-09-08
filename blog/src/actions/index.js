847320c443acb0f4db60fe3cdbd62e4d8cdbc299
Thomas Lu <thomaslu@vt.edu>
2019-09-08 17:21:33 -0400

    Through lesson 189

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,16 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async dispatch => {
+    await dispatch(fetchPosts()); 
+};
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
     dispatch({ type: 'FETCH_POSTS', payload: response.data});
 };  
+
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data});
+};
--- blog/src/index.js
+++ blog/src/index.js
@@ -10,7 +10,7 @@ import reducers from './reducers';
 const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
         <App />
     </Provider>,
     document.querySelector('#root')

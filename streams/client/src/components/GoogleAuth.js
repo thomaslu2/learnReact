72e2eca1cb63a9fdf3e7ea4e45449641f3150466
Thomas Lu <thomaslu@vt.edu>
2019-09-16 22:25:00 -0400

    Through lesson 218

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends React.Component{
     state = { isSignedIn: null };
@@ -11,22 +13,49 @@ class GoogleAuth extends React.Component{
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
                 this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+                this.auth.isSignedIn.listen(this.onAuthChange);
             });
         });
     }
 
+    onAuthChange = (isSignedIn) => {
+        if(isSignedIn) {
+            this.props.signIn();
+        } else {
+            this.props.signOut();
+        }
+    };
+
+    onSignInClick = () => {
+        this.auth.signIn();
+    };
+
+    onSignOutClick = () => {
+        this.auth.signOut();
+    };
+
     renderAuthButton() {
         if(this.state.isSignedIn === null) {
-            return <div>I don't know if we are signed in</div>;
+            return null;
         } else if (this.state.isSignedIn){
-            return <div>I am signed in</div>;
+            return (
+                <button onClick={this.onSignOutClick} className="ui red google button">
+                    <i className="google icon" />
+                    Sign Out
+                </button>
+            );
         } else {
-            return <div>I am not signed in</div>;
+            return (
+                <button onClick={this.onSignInClick} className="ui red google button">
+                    <i className="google icon" />
+                    Sign In With Google 
+                </button>
+            );
         }
     }
     render(){
-        return <div>Google Auth</div>;
+        return <div>{this.renderAuthButton()}</div>;
     }
 }
 
-export default GoogleAuth;
+export default connect(null, { signIn, signOut })(GoogleAuth);

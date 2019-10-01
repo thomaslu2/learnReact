77694674b4f048e58362a84db8a1daf3acd21fa0
Thomas Lu <thomaslu@vt.edu>
2019-09-30 22:20:10 -0400

    Through lesson 242

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
     renderInput({ input, label, meta }) {
@@ -14,7 +16,7 @@ class StreamCreate extends React.Component {
     }
 
     onSubmit(formValues){
-        console.log(formValues);
+        this.props.createStream(formValues);
     }
 
     render() {
@@ -40,7 +42,9 @@ const validate = (formValues) => {
     return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped); 
